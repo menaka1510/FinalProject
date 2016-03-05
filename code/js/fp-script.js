601c3d1afca4c2a6cd861dd9272b48b2c49072ee
menaka1510 <me.designgeek@gmail.com>
2016-03-05 15:04:00 +0800

    Version 1.1
    Updates on code in homepage & About page.
    
    Project Status: 30%

--- code/js/fp-script.js
+++ code/js/fp-script.js
@@ -3,47 +3,43 @@ window.onload = function() {
 
   $(document).ready(function() {
     event.preventDefault();
-    var myArr = ['I create beautiful interfaces','I build great user experiences','I can make brands interesting','I love grids','I have quirky ideas','I bake tasty cupcakes']; // Categories
-
-    for (i = 0; i < myArr.length; i++) { // For loop from position 0 to 5 of the array
-      $('#make-stuff').append('<option>' + myArr[i] + '</option>'); //appends the categories to the dropdown
-    }
-
-    $('#make-stuff').change(function(event) {
-      var selected = $("#make-stuff").val();
+var myArr = ['I create beautiful interfaces','I build great user experiences','I can make brands interesting','I love grids','I have quirky ideas','I bake tasty cupcakes']; // Categories
+for (i = 0; i < myArr.length; i++) { // For loop from position 0 to 6 of the array
+$('#make-stuff').append('<option>' + myArr[i] + '</option>'); //appends the categories to the dropdown
+}
+    $('#make-stuff').change(function(event) { //On selection change
+        var selected = $("#make-stuff").val();
 
       //Multiple cases for each background
       switch (selected) {
         case 'I create beautiful interfaces':
-          $('.hero-container').attr('class','ui');
+          $('.hero-container').addClass('ui').removeClass('ux','branding','print','illustration','cupcakes','default');
           break;
 
         case 'I build great user experiences':
-          $('.hero-container').attr('class','ux');
+          $('.hero-container').addClass('ux').removeClass('ui','branding','print','illustration','cupcakes','default');
           break;
 
         case 'I can make brands interesting':
-          $('.hero-container').attr('class','branding');
+          $('.hero-container').addClass('branding').removeClass('ux','ui','print','illustration','cupcakes','default');
           break;
 
         case 'I love grids':
-          $('.hero-container').attr('class','print');
+          $('.hero-container').addClass('print').removeClass('ux','branding','ui','illustration','cupcakes','default');
           break;
 
         case 'I have quirky ideas':
-          $('.hero-container').attr('class','illustration');
+          $('.hero-container').addClass('illustration').removeClass('ux','branding','print','ui','cupcakes','default');
           break;
 
         case 'I bake tasty cupcakes':
-          $('.hero-container').attr('class','cupcakes');
+          $('.hero-container').addClass('cupcakes').removeClass('ux','branding','print','illustration','ui','default');
           break;
 
         default:
-          $('.hero-container').attr('class','default');
+          $('.hero-container').addClass('default').removeClass('ui','ux','branding','print','illustration','cupcakes');
           break;
-
-      }
-
-    })
+          }
+      })
   })
 }
